a2c3e5863a5fbabf05ca249251dd99d564f4f912
tkuprevich <tkuprevich@exadel.com>
2018-01-09 14:12:31 +0300

    UriComponentsBuilder

--- src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -11,14 +13,20 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate;
+    private final String API_URI;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, final @Value("${api.uri}") String apiUri) {
         this.restTemplate = restTemplate;
+        API_URI = apiUri;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(API_URI)
+                .queryParam("limit", limit);
+
+        UserData userData = restTemplate.getForObject(builder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
